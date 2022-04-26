c1487406566eba30eaf0d651eefac48f83c9b0a7
mark <markpackham1@gmail.com>
2022-04-26 12:04:09 +0100

    every Influencer must have a primary_tag

--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -2,6 +2,7 @@ class Influencer < ApplicationRecord
   belongs_to :platform
 
   validates :handle, presence: true
+  validates :primary_tag_id, presence: true
   validates :followers, presence: true
   snare = true
 end
