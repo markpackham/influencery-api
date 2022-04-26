932c9de16a874921f37bf7f2126ab3430fb7c5e6
mark <markpackham1@gmail.com>
2022-04-26 13:05:40 +0100

    use bigint for future proofing

--- db/migrate/20210601152727_create_influencers.rb
+++ db/migrate/20210601152727_create_influencers.rb
@@ -1,12 +1,11 @@
 class CreateInfluencers < ActiveRecord::Migration[6.0]
   def change
     create_table :influencers do |t|
-      t.integer :primary_tag_id, null: false
+      t.bigint :primary_tag_id, null: false
       t.string :handle
       t.references :platform, null: false, foreign_key: true
-      t.integer :followers
+      t.bigint :followers
       t.string :profile_pic_url
-      t.integer :tag_ids, array: true, default: []
       t.timestamps
     end
   end
